1dce0f87c05837c51226f112404d6f69db3bad36
gr-b <grbishop@wpi.edu>
2016-09-22 08:51:02 -0700

    Added submission folder, including the new system files necessary for recompilation of the kernel.

---
+++ submission/readme.txt
@@ -0,0 +1,14 @@
+Authors: Griffin Bishop and Ian Vossoughi
+Username: grbishop
+
+Notes:
+The assignment says to use the file "syscall_table_32.S"
+and the file "unistd_32.h" while neither of these files
+exist. Later on, the assigment uses "syscall_32.tbl" and
+"syscalls.h" instead of the previous files, so I assumed 
+that these should be used instead. 
+These files are included in the submission instead of 
+the other ones (I assume this is the way it should be).
+
+The testcalls.c file is simply the proj2test.c file
+from the course website with the name changed. 
---
+++ submission/testcalls.c
@@ -0,0 +1,25 @@
+// Project 2 Pre-Project - Griffin Bishop and Ian Vossoughi
+#include <sys/syscall.h>
+#include <stdio.h>
+
+// These values MUST match the syscall_32.tbl modifications:
+#define __NR_cs3013_syscall1 355
+#define __NR_cs3013_syscall2 356
+#define __NR_cs3013_syscall3 357
+
+long testCall1 ( void) {
+        return (long) syscall(__NR_cs3013_syscall1);
+}
+long testCall2 ( void) {
+        return (long) syscall(__NR_cs3013_syscall2);
+}
+long testCall3 ( void) {
+        return (long) syscall(__NR_cs3013_syscall3);
+}
+int main () {
+        printf("The return values of the system calls are:\n");
+        printf("\tcs3013_syscall1: %ld\n", testCall1());
+        printf("\tcs3013_syscall2: %ld\n", testCall2());
+        printf("\tcs3013_syscall3: %ld\n", testCall3());
+        return 0;
+}
